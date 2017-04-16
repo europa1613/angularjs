c5bf61b820256fd7cd4eac4b36b9b0dd3aec8ed5
Arvind <europa1613@users.noreply.github.com>
2017-04-16 19:45:16 -0400

    Dependency Injection, Scope Service Injection into Controller

--- 01-alto-ud/1-module-app-controller/app.js
+++ 01-alto-ud/1-module-app-controller/app.js
@@ -2,6 +2,13 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', function() {
+angularApp.controller('mainController', function($scope) {
+	console.log($scope);
+	console.log('hello');
 
+	$scope.name = 'John, Snow';
+	$scope.occupation = 'Crow';
+	$scope.getName = function() {
+		return 'John, Snow';
+	}
 });
