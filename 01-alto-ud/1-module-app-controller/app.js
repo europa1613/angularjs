cf4cafc0939b007eb4a3155b3fa7450840cc04c2
Arvind <europa1613@users.noreply.github.com>
2017-04-16 22:09:29 -0400

    DI and Minification problem

--- 01-alto-ud/1-module-app-controller/app.js
+++ 01-alto-ud/1-module-app-controller/app.js
@@ -18,3 +18,34 @@ angularApp.controller('mainController', function($scope, $log, $filter) {
 
 
 });
+
+/* *********************************
+ * Minification Problem
+ * *********************************
+ */
+angularApp.controller('ForMinifierController', function($log, $scope) {
+    $log.info($scope);
+});
+
+/* $log and $scope are renamed to n and o, 
+ * which makes them not inject by angular
+ * Finally failing the function
+ */
+//angularApp.controller("ForMinifierController", function(n, o) { n.info(o) });
+
+/* *********************************
+ * Minification Problem
+ * *********************************
+ */
+
+/*
+ * To Avoid above problem,  
+ * Angular provide different syntax to setup controllers 
+ * NOTE: ORDER of injects and fn parameters MATTER.
+ */
+
+angularApp.controller('ForMinifierController', ["$log", '$scope', function($log, $scope) {
+    $log.info($scope);
+}]);
+
+angularApp.controller("ForMinifierController", ["$log", "$scope", function(o, n) { o.info(n) }]);
