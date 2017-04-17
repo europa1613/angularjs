304a905afc537bd29f5c06e3ff2780859b0ee528
Arvind <europa1613@users.noreply.github.com>
2017-04-16 20:05:03 -0400

    angular.injector().annotate(somefn): Angular DI

--- 01-alto-ud/1-module-app-controller/app.js
+++ 01-alto-ud/1-module-app-controller/app.js
@@ -3,12 +3,19 @@ var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
 angularApp.controller('mainController', function($scope) {
-	console.log($scope);
-	console.log('hello');
+    console.log($scope);
+    console.log('hello');
 
-	$scope.name = 'John, Snow';
-	$scope.occupation = 'Crow';
-	$scope.getName = function() {
-		return 'John, Snow';
-	}
+    $scope.name = 'John, Snow';
+    $scope.occupation = 'Crow';
+    $scope.getName = function() {
+        return 'John, Snow';
+    }
 });
+
+function add(a, b, $scope) {
+    return a + b;
+}
+console.log(add.toString());
+
+console.log(angular.injector().annotate(add)); // ["a", "b", "$scope"]
