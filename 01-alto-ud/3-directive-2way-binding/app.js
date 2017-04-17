6abad61c6d0e89ab68bd70ec57f63bf9c0661622
Arvind <europa1613@users.noreply.github.com>
2017-04-16 23:54:08 -0400

    , digest loop

--- 01-alto-ud/3-directive-2way-binding/app.js
+++ 01-alto-ud/3-directive-2way-binding/app.js
@@ -5,5 +5,12 @@ angularApp.controller('MainController', ['$scope', '$timeout', '$filter', functi
     $scope.occupation = '';
     $scope.occuCaps = function() {
         return $filter('uppercase')($scope.occupation);
-    }
+    };
+
+    $scope.$watch('occupation', function(newValue, oldValue) {
+    	console.log('changed!');
+    	console.log('oldValue: ', oldValue);
+    	console.log('newValue: ', newValue);
+    });
+
 }]);
