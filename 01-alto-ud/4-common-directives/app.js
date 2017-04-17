25e49e74eee1885462e926ea2d0e3084fc0f32a0
Arvind <europa1613@users.noreply.github.com>
2017-04-17 00:41:32 -0400

    ng-if, ng-show, ng-hide, ng-class, ng-repeat

--- 01-alto-ud/4-common-directives/app.js
+++ 01-alto-ud/4-common-directives/app.js
@@ -7,11 +7,21 @@ angularApp.controller('MainController', ['$scope', '$timeout', '$filter', functi
         return $filter('uppercase')($scope.occupation);
     };
 
-    $scope.$watch('occupation', function(newValue, oldValue) {
+    $scope.limitChars = 5;
+
+    $scope.rules = [{
+        ruleName: 'You must comply with all the rules.'
+    }, {
+        ruleName: 'You must comply with all the rules.'
+    }, {
+        ruleName: 'You must comply with all the rules.'
+    }];
+
+    /*$scope.$watch('occupation', function(newValue, oldValue) {
         console.log('changed!');
         console.log('oldValue: ', oldValue);
         console.log('newValue: ', newValue);
-    });
+    });*/
 
     /*
      * if model value changed outside of angular context, 
@@ -19,11 +29,11 @@ angularApp.controller('MainController', ['$scope', '$timeout', '$filter', functi
      * ELSE USE $timeout
      */
 
-    setTimeout(function() {
+    /*setTimeout(function() {
         $scope.$apply(function() {
             $scope.occupation = 'setTimeout Change';
             console.log('scope changed from setTimeout');
         });
-    }, 3000);
+    }, 3000);*/
 
 }]);
