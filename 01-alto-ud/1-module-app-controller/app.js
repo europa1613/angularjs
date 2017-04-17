28655b905924cd432bdc0aac8a734b7a38ea4c02
Arvind <europa1613@users.noreply.github.com>
2017-04-16 21:01:23 -0400

    ngMessages

--- 01-alto-ud/1-module-app-controller/app.js
+++ 01-alto-ud/1-module-app-controller/app.js
@@ -1,5 +1,5 @@
 // MODULE
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngMessages']);
 
 // CONTROLLERS
 angularApp.controller('mainController', function($scope, $log, $filter) {
