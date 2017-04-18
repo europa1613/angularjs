81f23a1409fe65010c463bc36210d08f51bfbf74
Arvind <arvindkumarrukmaji@gmail.com>
2017-04-18 16:05:22 -0400

    User factory test setup

--- 02-scotchio-angular-karma-jasmine/meet-irl/app/services/users/users.spec.js
+++ 02-scotchio-angular-karma-jasmine/meet-irl/app/services/users/users.spec.js
@@ -1,5 +1,20 @@
 describe('Users factory', function() {
-	it('dummy test, 2+2=4', function() {
-		expect().toEqual(4);
-	});
+    var Users;
+
+    // Before each test load our api.users module
+    beforeEach(angular.mock.module('api.users'));
+
+    // Before each test set our injected Users factory (_Users_) to our local Users variable
+    beforeEach(inject(function(_Users_) {
+        Users = _Users_;
+    }));
+
+    it('dummy test, 2+2=4', function() {
+        expect(2 + 2).toEqual(4);
+    });
+
+    // A simple test to verify the Users factory exists
+    it('should be defined', function() {
+        expect(Users).toBeDefined();
+    });
 });
