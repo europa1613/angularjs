e971f982a9a7a20b25624ebbb4d7f85fa927b474
Arvind <europa1613@users.noreply.github.com>
2017-04-21 00:49:44 -0400

    Initial service with name not persisting problem on input change

---
+++ 01-alto-ud/9-angular-service/app.js
@@ -0,0 +1,34 @@
+var angularApp = angular.module('angularApp', ['ngRoute']);
+
+angularApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'MainController'
+        })
+        .when('/second/:param1', { //second' route will not work but /second/123 works
+            templateUrl: 'pages/second.html',
+            controller: 'SecondController'
+        })
+});
+
+angularApp.service('MainService', function() {
+    var self = this;
+    this.name = 'John';
+    this.lastname = 'Snow';
+    this.getFullName = function() {
+        return self.name + ', ' + self.lastname;
+    }
+});
+
+angularApp.controller('MainController', ['$scope', '$location', '$log', 'MainService', function($scope, $location, $log, mainService) {
+    $scope.name = mainService.name;
+    $log.warn($location.path());
+    $log.info(mainService.getFullName());
+}]);
+
+angularApp.controller('SecondController', ['$scope', '$location', '$log', '$routeParams', 'MainService', function($scope, $location, $log, $routeParams, mainService) {
+    $scope.name = mainService.name;
+    $log.warn($location.path());
+    $scope.param1 = $routeParams.param1;
+}]);
