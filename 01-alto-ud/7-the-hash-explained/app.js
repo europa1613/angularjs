601b9827318143e5af4f3e4b6582c0efc8146e8b
Arvind <europa1613@users.noreply.github.com>
2017-04-20 23:02:35 -0400

    Simulating virtual routes with Hash

--- 01-alto-ud/7-the-hash-explained/app.js
+++ 01-alto-ud/7-the-hash-explained/app.js
@@ -1,3 +1,12 @@
 window.addEventListener('hashchange', function() {
-	console.log('Hash changed! ', window.location.hash);
+    console.log('Hash changed! ', window.location.hash);
+    if (window.location.hash === '#/bookmark/1') {
+        console.log("Show page 1!");
+    } else if (window.location.hash === '#/bookmark/2') {
+        console.log("Show page 2!");
+    } else if (window.location.hash === '#/bookmark/3') {
+        console.log("Show page 3!");
+    } else if (window.location.hash === '#/bookmark/4') {
+        console.log("Show page 4!");
+    }
 });
