d9ad24019a0bbf0fe7414789d3b074f02eb1d498
Arvind <europa1613@users.noreply.github.com>
2017-04-21 00:06:46 -0400

    ng-view, ngRoute, routeProvide

--- 01-alto-ud/8-routing-templates-controllers/app.js
+++ 01-alto-ud/8-routing-templates-controllers/app.js
@@ -1,6 +1,23 @@
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngRoute']);
+
+angularApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'MainController'
+        })
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'SecondController'
+        })
+});
 
 angularApp.controller('MainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
-    $scope.name = 'Main';
+    $scope.name = 'Home';
+    $log.warn($location.path());
+}]);
+
+angularApp.controller('SecondController', ['$scope', '$location', '$log', function($scope, $location, $log) {
+    $scope.name = 'Second';
     $log.warn($location.path());
 }]);
