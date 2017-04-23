693a1935b094e29d5a65d38e4ee553c83a1c55b7
Arvind <europa1613@users.noreply.github.com>
2017-04-23 11:10:38 -0400

    2nd way - add a containing object to scope within each controller

--- 01-alto-ud/14-controllerAs/app.js
+++ 01-alto-ud/14-controllerAs/app.js
@@ -1,17 +1,21 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('parentController1', ['$scope', function($scope) {
-	$scope.message = 'Parent 1 message.';
+	var parent1vm = $scope.parent1vm = {};
+	parent1vm.message = 'Parent 1 message.';
 }]);
 
 myApp.controller('childController1', ['$scope', function($scope) {
-	$scope.message = 'Child 1 message.';
+	var child1vm = $scope.child1vm = {};
+	child1vm.message = 'Child 1 message.';
 }]);
 
 myApp.controller('parentController2', ['$scope', function($scope) {
-	$scope.message = 'Parent 2 message.';
+	var parent2vm = $scope.parent2vm = {};
+	parent2vm.message = 'Parent 2 message.';
 }]);
 
 myApp.controller('childController2', ['$scope', function($scope) {
-	$scope.message = 'Child 2 message.';
+	var child2vm = $scope.child2vm = {};
+	child2vm.message = 'Child 2 message.';
 }]);
