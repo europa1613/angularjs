2748d23c0b178b84ba2d2df3b43bbaa6e416a039
Arvind <europa1613@users.noreply.github.com>
2017-04-23 17:55:32 -0400

    replace controller with component and use it readily as a directive

--- 03-angular-1.5/app/blog-list/blog-list.component.js
+++ 03-angular-1.5/app/blog-list/blog-list.component.js
@@ -1,11 +1,24 @@
 angular.module('blogList')
-    .controller('BlogListController', function($scope) {
-        console.log('Hello Controller!');
-        $scope.title = 'Hi there!';
-        $scope.clicks = 0;
-        $scope.click = function() {
-            console.log('clicked');
-            $scope.clicks += 1;
-            $scope.title = 'Clicked ' + $scope.clicks + ' times!';
+    .component('blogList', {
+        template: '<div><h1> {{title}} </h1><button ng-click="click()">Click me</button></div>',
+        controller: function($scope) {
+            console.log('Hello Component!');
+            $scope.title = 'Hi there! Component Here!';
+            $scope.clicks = 0;
+            $scope.click = function() {
+                console.log('clicked');
+                $scope.clicks += 1;
+                $scope.title = 'Clicked ' + $scope.clicks + ' times!';
+            }
         }
     });
+/*.controller('BlogListController', function($scope) {
+    console.log('Hello Controller!');
+    $scope.title = 'Hi there!';
+    $scope.clicks = 0;
+    $scope.click = function() {
+        console.log('clicked');
+        $scope.clicks += 1;
+        $scope.title = 'Clicked ' + $scope.clicks + ' times!';
+    }
+});*/
