541adb59c6faa7c349ee91e4b419d5f6fce3907c
Arvind <europa1613@users.noreply.github.com>
2017-04-22 20:26:15 -0400

    Initial Commit

---
+++ 01-alto-ud/12-compile-link-transclude/app.js
@@ -0,0 +1,157 @@
+var angularApp = angular.module('angularApp', ['ngRoute']);
+
+angularApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/main', {
+            templateUrl: 'pages/main.html',
+            controller: 'MainController'
+        })
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'SecondController'
+        })
+        .when('/second/:param1', { //second' route will not work but /second/123 works
+            templateUrl: 'pages/second.html',
+            controller: 'SecondController'
+        })
+        /*
+         * /main in otherwise will automatically set the route to /#/main 
+         * localhost:8080 -> http://localhost:8080/#/main
+         */
+        .otherwise('/main', {
+            templateUrl: 'pages/main.html',
+            controller: 'MainController'
+        });
+});
+
+angularApp.service('MainService', function() {
+    var self = this;
+    this.name = 'Arya';
+    this.lastname = 'Stark';
+    this.getFullName = function() {
+        return self.name + ', ' + self.lastname;
+    }
+});
+
+angularApp.controller('MainController', ['$scope', '$location', '$log', 'MainService', function($scope, $location, $log, mainService) {
+    $scope.name = mainService.name;
+    $scope.page = 'Main';
+    $log.warn($location.path());
+    $log.info(mainService.getFullName());
+
+    $scope.$watch('name', function() {
+        mainService.name = $scope.name;
+    });
+
+    $scope.person = {
+        name: 'Arya, Stark',
+        address: '555 Main St',
+        city: 'Brooklyn',
+        state: 'NC',
+        zip: 1111
+    };
+
+    $scope.getFullAddr = function(person) {
+        return person.address + ', ' + person.city + ', ' + person.state + ' ' + person.zip;
+    };
+
+    $scope.people = [{
+        name: 'John, Snow',
+        address: '555 Main St',
+        city: 'Brooklyn',
+        state: 'NC',
+        zip: 1111
+    }, {
+        name: 'Rob, Stark',
+        address: '555 Main St',
+        city: 'Brooklyn',
+        state: 'NC',
+        zip: 1111
+    }, {
+        name: 'Rickon, Stark',
+        address: '555 Main St',
+        city: 'Brooklyn',
+        state: 'NC',
+        zip: 1111
+    }, {
+        name: 'Brandon, Stark',
+        address: '555 Main St',
+        city: 'Brooklyn',
+        state: 'NC',
+        zip: 1111
+    }];
+
+}]);
+
+angularApp.controller('SecondController', ['$scope', '$location', '$log', '$routeParams', 'MainService', function($scope, $location, $log, $routeParams, mainService) {
+    $scope.name = mainService.name;
+    $log.warn($location.path());
+
+    $scope.param1 = $routeParams.param1;
+
+    $scope.$watch('name', function() {
+        mainService.name = $scope.name;
+    });
+}]);
+
+
+
+/* @ = text binding or one way binding: value passed directly thru interpolation {{}}: meaning, this translates to person-name attr in search-result directive 
+ * and takes value as {{person.name}}, see main.html, 
+ * and in searchresult.html, the value can be used as {{personName}}
+ */
+angularApp.directive('searchResult', function() {
+    return {
+        restrict: 'AECM', //A=Attribute, E=Element, AE=both(default), C=Class value, M=HTML Comment
+        templateUrl: 'directives/searchresult.html',
+        replace: true,
+        scope: {
+            personName: '@',
+            personAddress: '@'
+        }
+    }
+});
+
+/*
+ * '=': 2-way binding, any changes will be reflected in the source
+ */
+angularApp.directive('searchResultTwo', function() {
+    return {
+        restrict: 'AECM', //A=Attribute, E=Element, AE=both(default), C=Class value, M=HTML Comment
+        templateUrl: 'directives/searchresult2.html',
+        replace: true,
+        scope: {
+            personObject: '='
+        }
+    }
+});
+
+/*
+ * & - passing a method/function in directive's scope
+ */
+angularApp.directive('searchResultFn', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/searchResult3.html',
+        replace: true,
+        scope: {
+            personObject: '=',
+            fullAddress: '&'
+        }
+    }
+});
+
+/*
+ * display people using custom directive and ng-repeat
+ */
+angularApp.directive('searchResultPeople', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/searchResult3.html',
+        replace: true,
+        scope: {
+            personObject: '=',
+            fullAddress: '&'
+        }
+    }
+});
