addd22a40ba4c26244d6a9016a395bb928681615
Arvind <europa1613@users.noreply.github.com>
2017-04-30 13:05:28 -0400

    first angular component - simple template

---
+++ 05-angular-1.5-component-ps/ps-movies/movie-list.component.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+    var module = angular.module('psMovies');
+
+    module.component('movieList', {
+    	template: '<h1>Hello from movieList component!</h1>'
+    });
+}());
