a9d16483b013da0078343eaa41cd33cb25c2f6e8
Arvind <europa1613@users.noreply.github.com>
2017-04-30 16:41:16 -0400

    Rating component

---
+++ 05-angular-1.5-component-ps/ps-movies/movie-rating.component.js
@@ -0,0 +1,27 @@
+(function() {
+    'use strict';
+
+    var module = angular.module('psMovies');
+
+    module.component('movieRating', {
+        templateUrl: 'ps-movies/movie-rating.component.html',
+        bindings: {
+            value: "<"
+        },
+        controllerAs: 'model',
+        controller: function() {
+            var model = this;
+
+            model.$onInit = function() {
+                model.entries = new Array(model.value);
+                console.log(model.entries);
+            };
+
+            model.$onChanges = function(changeObj) {
+            	console.log(changeObj);
+            	model.entries = new Array(model.value);
+            };
+        }
+    });
+
+}());
