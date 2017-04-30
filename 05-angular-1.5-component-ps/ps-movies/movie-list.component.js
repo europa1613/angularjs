22cfdeee7102cab5706f61d6e48a34390004d2b3
Arvind <europa1613@users.noreply.github.com>
2017-04-30 13:25:20 -0400

    controllerAs

--- 05-angular-1.5-component-ps/ps-movies/movie-list.component.js
+++ 05-angular-1.5-component-ps/ps-movies/movie-list.component.js
@@ -3,7 +3,8 @@
     var module = angular.module('psMovies');
 
     module.component('movieList', {
-    	templateUrl: '/ps-movies/movie-list.component.html', 
+    	templateUrl: '/ps-movies/movie-list.component.html',
+    	controllerAs: 'model',
     	controller: function() {
     		this.message = 'Hello from Component Controller!'
     	}
