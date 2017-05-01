97fc3ab78c9d2e7479354c5be79b6fd391d4013c
Arvind <europa1613@users.noreply.github.com>
2017-04-30 20:51:37 -0400

    classic angular router using ngRoute

--- 05-angular-1.5-component-ps/ps-movies/module.js
+++ 05-angular-1.5-component-ps/ps-movies/module.js
@@ -1,6 +1,21 @@
 (function() {
     "use strict";
 
-    angular.module("psMovies", []);
+    angular.module('psMovies', ['ngRoute'])
+        .config(function($routeProvider) {
+            $routeProvider
+                .when('/list', {
+                    template: '<movie-list></movie-list>'
+                })
+                .when('/about', {
+                    template: '<app-about></app-about>'
+                })
+                .otherwise({
+                    redirectTo: '/list'
+                })
+        })
+        .component('appAbout', {
+            template: 'This is about Movies'
+        });
 
 }());
