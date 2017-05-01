c9e60dedb33220dd3ee5d18d68c033e6d5a43e09
Arvind <europa1613@users.noreply.github.com>
2017-04-30 21:44:30 -0400

    Component Router: LifeCycle hooks: canActivate, routerOnActivate

--- 05-angular-1.5-component-ps/ps-movies/movie-details.component.js
+++ 05-angular-1.5-component-ps/ps-movies/movie-details.component.js
@@ -1,8 +1,23 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	angular.module('psMovies')
-		.component('movieDetails', {
-			templateUrl: 'ps-movies/movie-details.component.html',
-		});
+    angular.module('psMovies')
+        .component('movieDetails', {
+            templateUrl: 'ps-movies/movie-details.component.html',
+            /*$canActivate: function($timeout) {
+                return $timeout(function() {
+                    return true;
+                }, 3000);
+            },*/
+            controller: function() {
+                var model = this;
+
+                model.$routerOnActivate = function(nextRoute, prevRoute) {
+                	console.log(nextRoute);
+                	console.log(prevRoute);
+                	model.id = nextRoute.params.id;
+                };
+            },
+            controllerAs: 'model'
+        });
 }());
