d5d99f8e1ec9390c0b32368d4fe4a1f9c2c91cbe
Arvind <europa1613@users.noreply.github.com>
2017-07-16 23:39:34 -0400

    Service

--- 06-angularjs-jumpstart/FactoriesAndServices/app/controllers/ordersController.js
+++ 06-angularjs-jumpstart/FactoriesAndServices/app/controllers/ordersController.js
@@ -1,18 +1,18 @@
 (function() {
 
-    var OrdersController = function($scope, $routeParams, customersFactory) {
+    var OrdersController = function($scope, $routeParams, customersService) {
         var customerId = $routeParams.customerId;
         $scope.orders = null;
         $scope.customer = null;
 
         function init() {
-            $scope.customer = customersFactory.getCustomer(customerId);
+            $scope.customer = customersService.getCustomer(customerId);
         }
 
         init();
     };
 
-    OrdersController.$inject = ['$scope', '$routeParams', 'customersFactory'];
+    OrdersController.$inject = ['$scope', '$routeParams', 'customersService'];
 
     angular.module('customersApp')
         .controller('OrdersController', OrdersController);
